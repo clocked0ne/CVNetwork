2bd0d79ab7901ceec07bbecef0b88af7f6f2db2a
clocked0ne <webdevelopment@clocked0ne.co.uk>
2016-06-21 17:01:40 +0100

    We have a webpage!

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 var express   = require('express');
 var router    = express.Router();
+var request   = require('request');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -10,6 +11,20 @@ router.get('/', function (req, res, next) {
 
 router.get('/dashboard', function (req, res, next) {
 	return res.render('dashboard', { view: { dashboard: 1 } });
+});
+
+router.post('/savecv', function (req, res, next) {
+	request.post({
+		url: 'http://192.168.2.54:8080/api/cvdata/add',
+		json: true,
+		body: req.body
+	}, function (error, response, body) {
+		if (error) {
+			console.dir(error);
+			return res.status(500).send(error);
+		}
+		return res.send(body);
+	});
 });
 
 module.exports = router;
--- src/js/semantic-ui.js
+++ src/js/semantic-ui.js
@@ -1,3 +1,194 @@
+var historyCount = 1,
+	submitCV = null,
+	entries = {};
+	entries.isLooking = 'false';
+	entries.phone = null;
+	entries.address = null;
+	entries.email = null;
+	entries.jobtitle = null;
+	entries.summary = null;
+	entries.history1 = null;
+	entries.history2 = null;
+	entries.history3 = null;
+	entries.history4 = null;
 $(function() {
 	$('.ui.left.sidebar').sidebar('setting', 'transition', 'uncover').sidebar('attach events', '.toc.item');
+	$('.ui.checkbox').checkbox({
+		onChecked: function () {
+			entries.isLooking = 'true';
+		},
+		onUnchecked: function () {
+			entries.isLooking = 'false';
+		}
+	});
+	$.sanitize = function(input) {
+		var output = input.replace(/<script[^>]*?>.*?<\/script>/gi, '').
+		replace(/<[\/\!]*?[^<>]*?>/gi, '').
+		replace(/<style[^>]*?>.*?<\/style>/gi, '').
+		replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '');
+		return output;
+	};
+	$('#main')
+		.on('click', '.edit', function (){
+			var $item = $(this),
+				type = $item.data('type'),
+				text = $item.children('.userContent').text().trim(),
+				trimmedContent = !text.match(/unknown/gi) ? text : '',
+				$input = $('<input />', {
+					'type': 'text',
+					'name': type + '-new',
+					'class': 'editInput',
+					'value': trimmedContent
+				});
+			if (!$item.children('input').length){
+				$item.append($input);
+				$input.focus();
+				$item.children('.userContent').hide();
+			}
+		})
+		.on('change', '.editInput', function (e){
+			e.stopImmediatePropagation();
+			var $input = $(this),
+				type = $input.attr('name').split('-')[0],
+				sanitisedInput = $.sanitize($input.val().trim());
+			if (sanitisedInput!=''){
+				submitCV = submitCV || { cvDataDetailList: [], jobHistory: [] };
+				if(type=='name') {
+					submitCV['firstname'] = sanitisedInput.split(' ')[0];
+					submitCV['lastname'] = sanitisedInput.split(' ')[1];
+				} else if (!isNaN(Number(type.slice(-1)))){
+					var position = 'history' + type.slice(-1);
+					var field = type.split('_')[1].slice(0, -1);
+					entries[position] = entries[position] || {};
+					entries[position].detail = 'WORKHISTORY';
+					entries[position].permission = 2;
+					entries[position]['value'] = entries[position]['value'] || [{}];
+		//			console.dir(entries[position]['value']);
+		//			console.dir(entries[position]['value'][0]);
+					entries[position]['value'][0][field] = sanitisedInput;
+
+		//			"value": [{
+		//				"date": "05/09/1989",
+		//				"title": "hello perfect"
+		//			}],
+
+				} else {
+					entries[type] = {
+						detail: type.toUpperCase(),
+						value: sanitisedInput,
+						permission: 1
+					}
+				}
+				$input.siblings('.userContent').text(sanitisedInput);
+			}
+		})
+		.on('focusout', '.editInput', function (e) {
+			e.stopImmediatePropagation();
+			var $input = $(this);
+			$input.siblings('.userContent').show();
+			$input.remove();
+		})
+		.on('click', '.editSummary', function (){
+			var $item = $(this),
+				type = $item.data('type'),
+				text = $item.children('.userContent').text().trim(),
+				trimmedContent = !text.match(/unknown/gi) ? text : '',
+				$input = $('<textarea />', {
+					'type': 'text',
+					'rows': '3',
+					'name': type + '-new',
+					'class': 'editInput'
+				});
+			$input.val(trimmedContent);
+			if (!$item.children('input').length){
+				$item.append($input);
+				$input.focus();
+				$item.children('.userContent').hide();
+			}
+		})
+		.on('change', '.editSummary', function (e){
+			e.stopImmediatePropagation();
+			var $note = $(this),
+				sanitisedNotes = $.sanitize($note.val().trim());
+			if (sanitisedNotes!='' || sanitisedNotes!='Notes'){
+				submitCV = submitCV || { cvDataDetailList: [], jobHistory: [] };
+				entries.summary = {
+					detail: type.toUpperCase(),
+					value: sanitisedNotes,
+					permission: 1
+				};
+			}
+		})
+		.on('click', '.addHistory', function (e){
+			if (historyCount < 4){
+				historyCount++;
+				$('<div class="event"><div class="content"><div class="summary"><span class="edit" data-type="previous_title' + historyCount
+					+ '"><span class="userContent">Previous Job title</span></span><div class="date edit" data-type="previous_date' + historyCount
+					+ '"><span class="userContent">November 2015</span></div></div><div class="extra text editSummary" data-type="previous_summary' + historyCount
+					+ '"><span class="userContent">Job Summary</span></div></div></div>')
+					.appendTo('.feed');
+				if (historyCount === 4)
+					$('.addHistory').remove();
+			}
+		})
+		.on('click', '.addCV', function (e){
+			e.preventDefault();
+			if (!submitCV
+				|| !submitCV.firstname
+				|| submitCV.firstname==''){
+				$.toast('You must enter your name', 4000, 'rounded');
+				return false;
+			}
+
+			submitCV.cvDataDetailList.push({
+				"detail": 'ISLOOKING',
+				"value": entries['isLooking'],
+				"permission": 1
+			});
+			if (entries.phone)
+				submitCV.cvDataDetailList.push(entries['phone']);
+			if (entries.address)
+				submitCV.cvDataDetailList.push(entries['address']);
+			if (entries.email)
+				submitCV.cvDataDetailList.push(entries['email']);
+			if (entries.jobtitle)
+				submitCV.cvDataDetailList.push(entries['jobtitle']);
+
+			if (entries.history1)
+				submitCV.jobHistory.push(entries['history1']);
+			if (entries.history2)
+				submitCV.jobHistory.push(entries['history2']);
+			if (entries.history3)
+				submitCV.jobHistory.push(entries['history3']);
+			if (entries.history4)
+				submitCV.jobHistory.push(entries['history4']);
+
+		//	console.log(JSON.stringify(submitCV));
+
+			$.ajax({
+				method: "POST",
+				url: "/savecv",
+				contentType: 'application/json;charset=utf-8',
+				data: JSON.stringify(submitCV),
+				dataType: 'json',
+				processData: false
+			})
+			.done(function (msg) {
+				console.log( "Data Saved: " + msg );
+				submitCV = { cvDataDetailList: [], jobHistory: [] };
+				entries.phone = null;
+				entries.address = null;
+				entries.email = null;
+				entries.jobtitle = null;
+				entries.summary = null;
+				entries.history1 = null;
+				entries.history2 = null;
+				entries.history3 = null;
+				entries.history4 = null;
+			}).fail(function (jqXHR, textStatus, errorThrown){
+				console.dir(textStatus);
+				console.dir(errorThrown);
+			});
+
+		});
 });
